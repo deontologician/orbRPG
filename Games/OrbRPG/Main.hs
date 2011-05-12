7ee67a9f2f3c0a40e05522f0aea11ecc41f15326
Josh Kuhn <kuhn.joshua@gmail.com>
2011-05-12 01:44:52 -0400

    Added the initial screen back in and now main uses the RPG monad

--- Games/OrbRPG/Main.hs
+++ Games/OrbRPG/Main.hs
@@ -1,28 +1,30 @@
 module Main where
 
-import System.Console.Haskeline
 import Types
 import Formatting
+import Control.Monad.State
 
 
 main :: IO ()
-main = runInputT defaultSettings loop
-    where loop :: InputT IO ()
-          loop = do
-            minput <- getInputLine "% "
-            case minput of
-              Nothing -> return ()
-              Just "quit" -> return ()
-              Just input -> do outputStrLn $ "Input was: " ++ input
-                               loop
+main = runRPG rpgLoop initState
 
--- do
---   putStr mainScreen
---   mainLoop dummyVal
 
--- These validate inputs and do the work of the game basically
-data Validator = Validator {
-      validate :: String -> Either (IO Validator) (IO String)}
+
+rpgLoop :: RPG ()
+rpgLoop = do
+  cs <- get
+  printOut initRound cs
+  minput <- getInputLine' "++ "
+  case minput of
+    Nothing -> return ()
+    Just "exit" -> return ()
+    Just input -> do outputStrLn' $ "Input was: " ++ input
+                     rpgLoop
+
+
+initState :: GameState
+initState = GameState{you = roughStart
+                     ,enemy = asalaStart}
 
 
 roughStart :: Player
@@ -53,15 +55,18 @@ asalaStart = Player{playerName = "Asala"
                    ,elemSlot = Nothing
                    ,techSlot = Nothing}
 
-{- Formatting Functions -}
+
+initRound :: Round
+initRound = Round{ printOut = const . outputStrLn' $ mainScreen }
 
 -- Welcome screen! (boxed)
 mainScreen :: String
 mainScreen = boxer $
-             ["    Welcome to the RPG game.",
+             ["    Welcome to the Orb RPG game.",
              "",
-             "       You are probably going to like   ",
-             "       this. Please check it out. OK?   ",
+             "       This game is an RPG with orbs    ",
+             "       You may or may not like the part ",
+             "       with the orbs.                   ",
              "                                        ",
              "      Select a Player:                  ",
              "        1. Roughgagh                    ",
